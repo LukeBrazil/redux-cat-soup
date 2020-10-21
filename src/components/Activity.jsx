e0df9f06ef2ff41e4a9b3f9cbed0ad2fb414f2a7
Luke Brazil <Luke.brazil13@icloud.com>
2020-10-21 10:55:56 -0500

    added redux?

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { connect } from 'react-redux';
+import { eat, play, nap } from '../redux/actions'
 
-const Activity = () => (
+const Activity = ( { activity, eat, play, nap }) => (
     <>
         <h1>Cat soup tastes good???</h1>
+        <p>The Cat is {activity}</p>
+        <button type='button' onClick={() => eat()}>Eating</button>
+        <button type='button' onClick={() => nap()}>Napping</button>
+        <button type='button' onClick={() => play()}>Playing</button>
     </>
 )
 
-export default Activity;
+const mapStateToProps = state => {
+    const { activity } = state;
+    return activity;
+}
+
+export default connect(mapStateToProps, { eat, play, nap })(Activity);
